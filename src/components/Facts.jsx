aeae8b8554cc01dc8ac4f311c492c0b8768471d3
ishakrupa31198@gmail.com <ishakrupa31198@gmail.com>
2022-11-03 12:50:37 +0530

    changesdone

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,21 +1,30 @@
 import React,{useState,useEffect} from "react";
 
 import './Facts.css';
-function Facts() {
-    const[quotes,setQuotes]=useState("");
+;
 
-    const getQuote =()=>{
-      fetch("https://catfact.ninja/fact")
-      .then((res)=>res.json())
-      .then((data)=>{
-    setQuotes(data)
-    
-      });
-    };
+function Facts (){
+    const[quotes,setQuotes]=useState([]);
+    let random=Math.floor(Math.random()*20)
+    const url =`https://catfact.ninja/facts?page=${random}`
     useEffect(()=>{
       getQuote();
     },[]);
-
+   
+    const getQuote =()=>{
+      fetch(url)
+      .then((res)=>res.json())
+      .then((data)=>{
+        
+        setQuotes(data.data)
+        
+        
+    
+      });
+     
+    };
+   
+    
    
   
   return (
@@ -23,18 +32,16 @@ function Facts() {
     <h2> RANDOM FACTS ABOUT MEOW </h2>
     <h3>!! Catch MICE :) Tastes NICE :!!</h3>
     <div className="App">
-      
-  <div className="quote"><span> HERE MY PAW PRINTS: </span>
-    <p> FunFact: {quotes.fact}</p>
-    <p> Length: {quotes.length}</p>
-    <div className ="btnContainer">
-      <button onClick= {getQuote}
-      className="btn"> Get Quote </button>
-   
-      
-      </div>
+
+      <ul>
+        {quotes.map((items,i )=>
+          <li key={i}>{items.fact}</li>
+        
+        )}
+      </ul>
     </div>
-    </div>
+
+  
     </>
   );
 }
